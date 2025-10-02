1e63c6b3fbbda80863ef7d98acdca0ced68ee582
SachithDimuthuu <Sachithdimuthu2001@gmail.com>
2025-10-02 16:40:26 +0530

    FIX

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,11 +25,9 @@
             <div class="flex justify-between items-center h-20">
                 <!-- Logo -->
                 <div class="flex items-center space-x-3">
-                    <div class="w-10 h-10 bg-gradient-to-br from-rose-500 to-rose-600 rounded-xl flex items-center justify-center shadow-lg">
-                        <svg class="w-6 h-6 text-white" fill="currentColor" viewBox="0 0 24 24">
-                            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm3.5-9c.83 0 1.5-.67 1.5-1.5S16.33 8 15.5 8 14 8.67 14 9.5s.67 1.5 1.5 1.5zm-7 0c.83 0 1.5-.67 1.5-1.5S9.33 8 8.5 8 7 8.67 7 9.5 7.67 11 8.5 11zm3.5 6.5c2.33 0 4.31-1.46 5.11-3.5H6.89c.8 2.04 2.78 3.5 5.11 3.5z"/>
-                        </svg>
-                    </div>
+                    <a href="{{ route('home') }}">
+                        <img src="{{ asset('images/Logo.jpg') }}" alt="Luxe Hair Studio Logo" class="h-10 w-auto">
+                    </a>
                     <a href="{{ route('home') }}" class="text-2xl font-bold text-gray-900 font-serif tracking-tight hover:text-rose-600 transition-colors duration-200">
                         Luxe Hair Studio
                     </a>
